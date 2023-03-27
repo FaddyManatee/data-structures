d3b08c6bf4f349b8e8bc51e14616ece8087aaabe
Thomas Morton <sc21tam@leeds.ac.uk>
2023-03-28 00:20:21 +0100

    Expose functions

--- lists/doubly/dlinkedlist.h
+++ lists/doubly/dlinkedlist.h
@@ -15,3 +15,10 @@ typedef struct DLinkedList {
     Node *end;   // Pointer to end of list.
     int   size;  // List size (number of nodes).
 } DLinkedList;
+
+DLinkedList* DLinkedList();
+Node* firstNode(DLinkedList *list);
+Node* lastNode(DLinkedList *list)
+void insertEnd(DLinkedList *list, Node *new);
+void insertAfter(DLinkedList *list, Node *node, Node *new);
+void printList(DLinkedList *list)
